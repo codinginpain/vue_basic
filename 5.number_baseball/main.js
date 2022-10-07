6b9ffddf6cfa32e08b62d115fec0b08ddea4f8b9
codinginpain <codinginpain@gmail.com>
2022-10-07 17:59:28 +0900

    vue loader

--- 5.number_baseball/main.js
+++ 5.number_baseball/main.js
@@ -1,3 +1,4 @@
-import Vue from 'vue';
+// import Vue from 'vue'; //vue3부턴 사용하지 않음
+import NumberBaseball from './NumberBaseball.vue';
 
-new Vue().$mount()
+new Vue(NumberBaseball).$mount('#root')
--- 5.number_baseball/webpack.config.js
+++ 5.number_baseball/webpack.config.js
@@ -1,15 +1,25 @@
+// const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const {VueLoaderPlugin} = require('vue-loader');
 const path = require("path"); //node에 들어있는 현재 경로를 받아 올 수 있는 script
 
 module.exports = {
+    mode:'development', //배포용은 production
+    devtool: 'eval', 
+    resolve: { 
+        extensions: ['.js', '.vue']
+    },
 	entry: {
         app:'./main.js',
 	},
     module: {
         rules: [{
-
+            test: /\.vue$/,
+            loader: 'vue-loader',
         }],
     },
-    plugins: [],
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
     output: {
         filename: 'app.js',
         path: path.join(__dirname, 'dist'),
